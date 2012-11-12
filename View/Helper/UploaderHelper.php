55280e02e1c8fb099b989e5854793ed4494fd9be
Grafikart <contact@grafikart.fr>
2012-11-12 18:07:03 +0100

    Force input type to textarea

--- View/Helper/UploaderHelper.php
+++ View/Helper/UploaderHelper.php
@@ -2,21 +2,21 @@
 class UploaderHelper extends AppHelper{
 
 	var $helpers = array('Html','Form');
-	var $javascript = false; 
+	var $javascript = false;
 
 	public function tinymce($field){
-		$model = $this->Form->_models; $model = key($model); 
-		$this->javascript(); 
-        $html = $this->Form->input($field,array('label'=>false,'class'=>'wysiwyg','style'=>'width:100%;height:500px','row' => 160));
+		$model = $this->Form->_models; $model = key($model);
+		$this->javascript();
+        $html = $this->Form->input($field,array('label'=>false,'class'=>'wysiwyg','style'=>'width:100%;height:500px','row' => 160, 'type' => 'textarea'));
     	if(isset($this->request->data[$model]['id'])){
 			$html .= '<input type="hidden" id="explorer" value="'.$this->Html->url('/admin/media/medias/index/'.$model.'/'.$this->request->data[$model]['id']).'/tinymce:1">';
     	}
-		return $html; 
+		return $html;
 	}
 
 	private function javascript(){
 		if($this->javascript){ return false; }
-		$this->javascript = true; 
+		$this->javascript = true;
 		$this->Html->script('/media/js/tinymce/tiny_mce.js',array('inline'=>false));
 	}
 
